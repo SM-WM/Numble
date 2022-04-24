022757cfdc5f831fac9c87848c5ac75c8bc36806
Walter Mwaniki <walter.g.mwaniki@gmail.com>
2022-04-23 21:31:24 -0400

    Initial Commit

---
+++ Numble/NumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbleApp.swift
+//  Numble
+//
+//  Created by Walter Mwaniki on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct NumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
